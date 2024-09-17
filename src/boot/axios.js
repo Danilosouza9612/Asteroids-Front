a3fa8f3b86410fac9ae8af8ac16e2136ce26dda8
danilosouza9612 <danilosouza9612@outlook.com>
2024-09-16 23:23:52 -0300

    Deployment information

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { boot } from "quasar/wrappers";
 
-const api = axios.create({ baseURL: 'http://localhost:3000'})
+const api = axios.create({ baseURL: 'http://asteroids-api-485748506.us-east-1.elb.amazonaws.com:3000'})
 
 export default boot(({app}) => {
   app.config.globalProperties.$axios = axios;
